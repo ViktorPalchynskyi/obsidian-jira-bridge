98e23b040eeac36a4c752d088984230bd22902af
Viktor <palchinskij19@gmail.com>
2026-01-11 20:03:13 +0300

    refactor: create feature module directory structure

---
+++ src/features/index.ts
@@ -0,0 +1,6 @@
+export * from './ticket-creation';
+export * from './status-change';
+export * from './link-ticket';
+export * from './sync';
+export * from './project-comparison';
+export * from './settings';
---
+++ src/features/status-change/index.ts
@@ -0,0 +1,3 @@
+export * from './modals';
+export * from './services';
+export * from './types';
